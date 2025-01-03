0bc7b146c59a1a87b31dcf37ce2aa0a2870d60b8
Milton <mportillo080@gmail.com>
2025-01-03 15:51:43 -0600

    stuck

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+
+
 function toggleSpinner() {
     const spinner = document.getElementById("spinner");
     spinner.style.display = "inline-block"; // Show the spinner
 
-    // Simulate a loading process
-    setTimeout(() => {
-        window.location.href = "your-new-page.html"; // Redirect to another page
-    }, 2000); // This simulates a loading time of 2 seconds
-}
-
 function openMenu() {
 	document.body.classList += " menu--open"
    }
-   
-   function closeMenu() {
+function closeMenu() {
 	document.body.classList.remove('menu--open')
    }
 
+   const iconContainer = document.querySelector('.icon-container');
+   const boxIcon = document.querySelector('.nav__icon');
+   const xSign = document.querySelector('.fa-x');
+   iconContainer.addEventListener('click', () => {
    
+   if (boxIcon.style.opacity === '1') { 
+   boxIcon.style.opacity = '0';
+   xSign.style.opacity = '1';
+   xSign.style.visibility = 'visible';
+} 
+	else {
+   boxIcon.style.opacity = '1';
+   xSign.style.opacity = '0';
+   xSign.style.visibility = 'hidden';
+   }
+   });
+}  
