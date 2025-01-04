3912b14a978de93f461de71cf165cd586ba8cbaf
Milton <mportillo080@gmail.com>
2025-01-03 18:12:53 -0600

    yeah

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function toggleSpinner() {
     const spinner = document.getElementById("spinner");
     spinner.style.display = "inline-block"; // Show the spinner
@@ -12,20 +10,20 @@ function closeMenu() {
 	document.body.classList.remove('menu--open')
    }
 
-   const iconContainer = document.querySelector('.icon-container');
-   const boxIcon = document.querySelector('.nav__icon');
-   const xSign = document.querySelector('.fa-x');
-   iconContainer.addEventListener('click', () => {
-   
-   if (boxIcon.style.opacity === '1') { 
-   boxIcon.style.opacity = '0';
-   xSign.style.opacity = '1';
-   xSign.style.visibility = 'visible';
-} 
-	else {
-   boxIcon.style.opacity = '1';
-   xSign.style.opacity = '0';
-   xSign.style.visibility = 'hidden';
-   }
-   });
-  
+const iconContainer = document.querySelector('.icon-container');
+const boxIcon = document.querySelector('.nav__icon');
+const xSign = document.querySelector('.fa-x');
+
+iconContainer.addEventListener('click', () => {
+  if (boxIcon.style.opacity === '1') {
+    boxIcon.style.opacity = '0';
+    xSign.style.opacity = '1';
+    xSign.style.visibility = 'visible';
+    xSign.style.transform = 'rotate(0deg)'; 
+  } else {
+    boxIcon.style.opacity = '1';
+    xSign.style.opacity = '0';
+    xSign.style.visibility = 'hidden';
+    xSign.style.transform = 'rotate(90deg)'; 
+  }
+});
